ff4c82bd97f74aaf5cd5edd36f836719cefa2cca
Pavel Zabalotny <Gi1030109>
2018-09-12 16:55:43 +0300

    Вынес React-select в компонент

--- src/components/App.js
+++ src/components/App.js
@@ -2,39 +2,24 @@ import React from 'react';
 import ArticleList from "./ArticleList";
 import PropTypes from 'prop-types';
 import UserForm from "./UserForm";
-import Select from 'react-select';
+import ReactSelect from './ReactSelect';
 
 class App extends React.Component {
     static propTypes = {
         articles: PropTypes.array
     };
-    state = {
-        selectedOption: null
-    };
+
     render() {
         const {articles} = this.props;
-        const title = articles.map((item) => {
-            return {
-                value: item.title,
-                label: item.title
-            };
-        });
-        console.log(title);
-
         return (
             <React.Fragment>
                 <UserForm/>
-                <Select
-                    value={this.state.selectedOption}
-                    onChange={this.handleChange}
-                    options={title}
-                    isMulti
-                />
+                <ReactSelect articles={articles}/>
                 <ArticleList articles={articles}/>
             </React.Fragment>
         )
     }
-    handleChange = selectedOption => this.setState({selectedOption})
+
 }
 
 export default App
---
+++ src/components/ReactSelect.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Select from 'react-select';
+
+export default class ReactSelect extends Component {
+    state = {
+        selectedOption: null
+    };
+    render() {
+        const {articles} = this.props;
+        const title = articles.map((article) => {
+            return {
+                value: article.title,
+                label: article.title
+            };
+        });
+        console.log(title);
+
+        return (
+            <div>
+                <Select
+                    value={this.state.selectedOption}
+                    onChange={this.handleChange}
+                    options={title}
+                    isMulti
+                />
+            </div>
+        );
+    }
+    handleChange = selectedOption => this.setState({selectedOption})
+}
